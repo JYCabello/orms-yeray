7feb5a8a15734a6adec91fcf3e59dec5ea0ce679
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-01 14:55:28 +0100

    lazy or not lazy

--- efcore/EFCoreCaveats/EFCoreCaveats/Program.cs
+++ efcore/EFCoreCaveats/EFCoreCaveats/Program.cs
@@ -1,9 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using EFCoreCaveats;
 using Microsoft.EntityFrameworkCore;
 
-using var context = new MyDbContext();
-context.Database.Migrate();
+await using var context = new MyDbContext();
 
-Console.WriteLine("Hello, World!");
+// context.Parents.Add(new ParentEntity
+// {
+//     Name = "My parent entity"
+// });
+//
+// await context.SaveChangesAsync();
+//
+// var parent = await context.Parents.FirstAsync(p => p.Name == "My parent entity");
+//
+// context.Children.AddRange(
+//     new ChildEntity { Name = "children number one", ParentID = parent.ID },
+//     new ChildEntity { Name = "children number two", ParentID = parent.ID },
+//     new ChildEntity { Name = "children number three", ParentID = parent.ID }
+// );
+//
+// await context.SaveChangesAsync();
+
+var stopwatch = Stopwatch.StartNew();
+
+var parent = await context
+    .Parents
+    .FirstAsync(p => p.Name == "My parent entity");
+
+foreach (var child in parent.Children)
+{
+    Console.WriteLine(child.Name);
+}
+
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
