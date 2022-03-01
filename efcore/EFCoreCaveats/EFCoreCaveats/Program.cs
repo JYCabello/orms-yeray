5b51186f8f0e0f8cb3911cb21f0b22a806b46b7b
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-03-01 14:14:17 +0100

    ef core

--- efcore/EFCoreCaveats/EFCoreCaveats/Program.cs
+++ efcore/EFCoreCaveats/EFCoreCaveats/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+const string connectionString = "Server=localhost;Database=ormcaveats;User Id=sa;Password=abcd1234ABCD;";
+
+
 Console.WriteLine("Hello, World!");
